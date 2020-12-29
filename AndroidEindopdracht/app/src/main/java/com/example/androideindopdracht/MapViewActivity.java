949571835b319f8ea12db9069084f9f1ebea9b0e
Kasper <kh.adan@student.avans.nl>
2020-12-29 13:36:21 +0100

    Auto stash before merge of "feature-mainScreenLayout" and "origin/feature-mainScreenLayout"

--- AndroidEindopdracht/app/src/main/java/com/example/androideindopdracht/MapViewActivity.java
+++ AndroidEindopdracht/app/src/main/java/com/example/androideindopdracht/MapViewActivity.java
@@ -3,14 +3,46 @@ package com.example.androideindopdracht;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MapViewActivity extends AppCompatActivity {
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+
+public class MapViewActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view);
 
+        map = findViewById(R.id.osm_map);
+        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
 
+        ImageButton zoomIn = findViewById(R.id.map_zoom_in);
+        zoomIn.setOnClickListener(this);
+        ImageButton zoomOut = findViewById(R.id.map_zoom_out);
+        zoomOut.setOnClickListener(this);
+        ImageButton recenterMap = findViewById(R.id.map_recenter);
+        recenterMap.setOnClickListener(this);
+        ImageButton home = findViewById(R.id.map_home_button);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.map_zoom_in:
+                map.getController().zoomIn();
+                break;
+            case R.id.map_zoom_out:
+                map.getController().zoomOut();
+                break;
+            case R.id.map_recenter:
+                break;
+            case R.id.map_home_button:
+                break;
+        }
     }
 }
--- AndroidEindopdracht/app/src/main/java/com/example/androideindopdracht/SplashScreenActivity.java
+++ AndroidEindopdracht/app/src/main/java/com/example/androideindopdracht/SplashScreenActivity.java
@@ -1,25 +1,68 @@
 package com.example.androideindopdracht;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     private int splashScreenDuration = 3000;
+    private final int requestCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-        new Handler().postDelayed(() -> {
-            Intent i = new Intent(SplashScreenActivity.this, MapViewActivity.class);
+        handleNeededPermissions(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            new Handler().postDelayed(() -> {
+                Intent i = new Intent(SplashScreenActivity.this, MapViewActivity.class);
+                startActivity(i);
+                finish();
+            }, splashScreenDuration);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            Intent i = new Intent(SplashScreenActivity.this, NavigationViewActivity.class);
             startActivity(i);
             finish();
-        }, splashScreenDuration);
+        }
+    }
+
+    private void handleNeededPermissions(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    requestCode);
+        }
     }
 }
