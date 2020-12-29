11d26b2c8acc1ddd3f4d4cd0ef3cde4afa3a6da9
Guilliam Lutz <guilliamlutz@outlook.com>
2020-12-29 13:35:32 +0100

    Added onclicklisteners for the buttons and changed settings button

--- AndroidEindopdracht/app/src/main/java/com/example/androideindopdracht/NavigationViewActivity.java
+++ AndroidEindopdracht/app/src/main/java/com/example/androideindopdracht/NavigationViewActivity.java
@@ -1,4 +1,59 @@
 package com.example.androideindopdracht;
 
-public class NavigationViewActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NavigationViewActivity extends AppCompatActivity {
+    private ImageButton profileButton;
+    private ImageButton backButton;
+    private ImageButton mapButton;
+    private ImageButton playButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_view);
+        this.profileButton = findViewById(R.id.profileButton);
+        this.backButton = findViewById(R.id.backButton);
+        this.mapButton = findViewById(R.id.mapButton);
+        this.playButton = findViewById(R.id.playButton);
+
+        this.profileButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(NavigationViewActivity.this, ProfileViewActivity.class);
+                startActivity(i);
+            }
+        });
+
+        this.backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(NavigationViewActivity.this, ProfileViewActivity.class);
+                startActivity(i);
+            }
+        });
+
+        this.mapButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(NavigationViewActivity.this, MapViewActivity.class);
+                startActivity(i);
+            }
+        });
+
+        this.playButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(NavigationViewActivity.this, MapViewActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
 }
---
+++ AndroidEindopdracht/app/src/main/java/com/example/androideindopdracht/ProfileViewActivity.java
@@ -0,0 +1,4 @@
+package com.example.androideindopdracht;
+
+public class ProfileViewActivity {
+}
